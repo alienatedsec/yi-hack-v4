941fa0ac9fdd64c4f8e7136aad1a8b19399c61bd
alienatedsec <73167064+alienatedsec@users.noreply.github.com>
2021-02-20 08:09:03 +0000

    [Fix] RTSP - High and Low resolution stream

--- src/static/static/home/yi-hack-v4/script/system.sh
+++ src/static/static/home/yi-hack-v4/script/system.sh
@@ -81,8 +81,10 @@ if [[ $(get_config RTSP) == "yes" ]] ; then
 #    fi
 
 if [[ -f "$YI_HACK_PREFIX/bin/h264grabber" && -f "$YI_HACK_PPREFIX/bin/rRTSPServer" ]] ; then
-    h264grabber -f &
+    h264grabber_l -f &
+    h264grabber_h -f &
     RRTSP_RES=both rRTSPServer &
+
 fi
 fi
 

7cfda2d5dd9dc86a95beacef62ac15400acac24f
alienatedsec <73167064+alienatedsec@users.noreply.github.com>
2021-03-29 00:06:21 +0100

    [Change] Moving Files to SD

--- src/dropbear/localoptions.h
+++ src/dropbear/localoptions.h
@@ -1,8 +1,12 @@
 #ifndef DROPBEAR_LOCALOPTIONS_H
 #define DROPBEAR_LOCALOPTIONS_H
 
-#define DSS_PRIV_FILENAME "/home/yi-hack-v4/etc/dropbear/dropbear_dss_host_key"
-#define RSA_PRIV_FILENAME "/home/yi-hack-v4/etc/dropbear/dropbear_rsa_host_key"
-#define ECDSA_PRIV_FILENAME "/home/yi-hack-v4/etc/dropbear/dropbear_ecdsa_host_key"
+#define DSS_PRIV_FILENAME "/tmp/sd/yi-hack-v4/etc/dropbear/dropbear_dss_host_key"
+#define RSA_PRIV_FILENAME "/tmp/sd/yi-hack-v4/etc/dropbear/dropbear_rsa_host_key"
+#define ECDSA_PRIV_FILENAME "/tmp/sd/yi-hack-v4/etc/dropbear/dropbear_ecdsa_host_key"
+
+#define DROPBEAR_PATH_SSH_PROGRAM "/tmp/sd/yi-hack-v4/bin/dbclient"
+
+#define DEFAULT_PATH "/usr/bin:/usr/sbin:/bin:/sbin:/tmp/sd/yi-hack-v4/bin:/tmp/sd/yi-hack-v4/sbin:/tmp/sd/yi-hack-v4/usr/bin:/tmp/sd/yi-hack-v4/usr/sbin"
 
 #endif /* DROPBEAR_LOCALOPTIONS_H */
